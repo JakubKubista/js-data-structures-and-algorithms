8037349178149e31f4e630caef8cb7d79a0ccef8
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-05-14 23:08:34 +0200

    Add Factorial test

--- programming-exercises/factorial.js
+++ programming-exercises/factorial.js
@@ -9,3 +9,9 @@ function Factorial(num) {
     if (num === 0) return 1;
     return num * Factorial(num - 1);
 }
+
+function test() {
+    console.log("Factorial: 5");
+    console.log(Factorial(5));
+}
+test()
