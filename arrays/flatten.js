3f8f5048ad0210c5e38d4ba5f9123451f4889d9b
Martin Záklasník <zaklasnik.martin@allrisk.cz>
2019-05-14 12:39:46 +0200

    Add flatten functions and test

--- arrays/flatten.js
+++ arrays/flatten.js
@@ -1,5 +1,21 @@
 "use strict"
 
+/**
+ * Flattening an array
+ * Example: [1,2,3,[1,2,3,4, [2,3,4]]] => [1, 2, 3, 1, 2, 3, 4, 2, 3, 4]
+ */
+
+
+/**
+ * Returns flatten array (recursive)
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+ * @param {array} arr
+ * @param {number} depth
+ */
+function flatten(arr, depth) {
+    return arr.flat(depth);
+}
+
 function flatten(oldArr) {
     var newArr = []
     for (var i = 0; i < oldArr.length; i++) {
@@ -10,4 +26,16 @@ function flatten(oldArr) {
         }
     }
     return newArr;
+}
+
+function flattenDeep(arr1) {
+    return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+}
+
+function test() {
+    const arr = [1, 2, 3, [1, 2, 3, 4, [2, 3, 4]]];
+    console.log("Input data: [1,2,3,[1,2,3,4, [2,3,4]]]");
+    console.log(flatten(arr, 2));
+    console.log(flatten(arr));
+    console.log(flattenDeep(arr));
 }
