ea23c906ad99b0745373def7246f2a097564301f
kubistaja <kubista.jakub@allrisk.cz>
2019-05-14 16:35:05 +0200

    Add node

---
+++ lists/cloneWithPointers.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Clone a linked list with next and random pointer.
+ * Time Complexity: O(n)
+ */
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data,
+            this.next = next
+    }
+}
