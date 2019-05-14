97e4970c57064b7be97b372076dd1296cf64d5ee
Jakub Kubišta <xkubista@gmail.com>
2019-05-15 00:56:31 +0200

    Add palindrome

---
+++ src/strings/palindrome.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * Check if is a string palindrome.
+ * Time Complexity: O(n / 2)
+ * @param {string} str
+ */
+function isPalindrome(str) {
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+
+function test() {
+    console.log(isPalindrome('isomorphic'))
+}
