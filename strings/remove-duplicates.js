a6e0b56f6093acb2810a606695e23f7e23f5aa85
kubistaja <kubista.jakub@allrisk.cz>
2019-05-14 12:48:44 +0200

    Add remove duplicates of string basic method

---
+++ strings/remove-duplicates.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * Returns string without duplicates of characters.
+ * Fast way to implement, but not the best way.
+ * Better way is to use BST or Hash table
+ * Time Complexity: O(n*n)
+ * @param {string} str
+ */
+function removeDuplicates(str) {
+    let newStr = '';
+    // Array passage - O(n)
+    for (let i = 0; i < str.length; i++) {
+        let character = str.charAt(i);
+        // Search character index of string - O(n)
+        if (newStr.indexOf(character) < 0) {
+            newStr += character;
+        }
+    }
+    return newStr;
+}
+function testRemoveDuplicates() {
+    let expected = 'geksfor';
+    let str = "geeksforgeeks";
+    console.log(removeDuplicates(str))
+    if (removeDuplicates(str) === expected) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+testRemoveDuplicates();
