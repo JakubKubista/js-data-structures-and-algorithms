05238c5f0f822c6dbd313dd73aee6b61e23ff703
kubistaja <kubista.jakub@allrisk.cz>
2019-05-14 17:46:43 +0200

    Add clone method for linked list

--- lists/linkedListDoublyClone.js
+++ lists/linkedListDoublyClone.js
@@ -1,15 +1,16 @@
 "use strict";
 
 /**
- * Clone a linked list with next and random pointer.
+ * Clone a doubly linked list with next and random pointer.
  * Time Complexity: O(n)
  * Source: https://practice.geeksforgeeks.org/problems/clone-a-linked-list-with-next-and-random-pointer/1
  */
 
 class Node {
-    constructor(data, next = null) {
+    constructor(data, next = null, random = null) {
         this.data = data;
         this.next = next;
+        this.random = random;
     }
 }
 
@@ -18,13 +19,43 @@ class LinkedList {
         this.head = null;
     }
 
-    cloneLinkedList() {
+    clone() {
+        let originalCurr = this.head;
+        let clonedCurr = null;
 
+        // Hash map which contains node to node mapping of
+        // original and clone linked list
+        let map = new Map(originalCurr, clonedCurr)
+
+        // Traverse the original list and make a copy of that.
+        // in the clone linked list.
+        while (originalCurr != null) {
+            clonedCurr = new Node(originalCurr.data);
+            map[originalCurr] = clonedCurr;
+            originalCurr = originalCurr.next;
+        }
+
+        // Adjusting the original list reference again.
+        originalCurr = this.head;
+
+        // Traversal of original list again to adjust the next
+        // and random references of clone list using hash map.
+        while (originalCurr != null) {
+            clonedCurr = map.get(originalCurr);
+            clonedCurr.next = map.get(originalCurr.next);
+            clonedCurr.random = map.get(originalCurr.random);
+            originalCurr = originalCurr.next;
+        }
+
+        // Return the head reference of the clone list.
+        return new LinkedList(map.get(this.head));
     }
 }
 
 function testLinkedList() {
-    let list = new LinkedList();
-    list.cloneLinkedList();
+    let listNew = new LinkedList();
+    let listCloned = listNew.clone();
+    console.log(listNew, listNew)
+    console.log(listNew === listCloned)
 }
 testLinkedList();
