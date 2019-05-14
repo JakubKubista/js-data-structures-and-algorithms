afbcbb5f6a495a6ced1186df1f5585712b704ba0
kubistaja <kubista.jakub@allrisk.cz>
2019-05-14 18:42:54 +0200

    Add complete clone double linked list

--- lists/linkedListDoublyClone.js
+++ lists/linkedListDoublyClone.js
@@ -15,23 +15,31 @@ class Node {
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+    constructor(head) {
+        this.head = head;
+    }
+
+    // Push at the head.
+    push(data) {
+        let node = new Node(data);
+        node.next = this.head;
+        this.head = node;
     }
 
     clone() {
+        let cp = this.head;
         let originalCurr = this.head;
         let clonedCurr = null;
 
         // Hash map which contains node to node mapping of
         // original and clone linked list
-        let map = new Map(originalCurr, clonedCurr)
+        let map = new Map();
 
         // Traverse the original list and make a copy of that.
         // in the clone linked list.
-        while (originalCurr != null) {
+        while (originalCurr !== undefined && originalCurr !== null) {
             clonedCurr = new Node(originalCurr.data);
-            map[originalCurr] = clonedCurr;
+            map.set(originalCurr, clonedCurr);
             originalCurr = originalCurr.next;
         }
 
@@ -40,10 +48,17 @@ class LinkedList {
 
         // Traversal of original list again to adjust the next
         // and random references of clone list using hash map.
-        while (originalCurr != null) {
+        while (originalCurr !== undefined && originalCurr !== null) {
             clonedCurr = map.get(originalCurr);
+
             clonedCurr.next = map.get(originalCurr.next);
             clonedCurr.random = map.get(originalCurr.random);
+
+            // Map changes null pointer into undefiend,
+            // but we works with null.
+            clonedCurr.next = undefinedToNull(clonedCurr.next);
+            clonedCurr.random = undefinedToNull(clonedCurr.random);
+
             originalCurr = originalCurr.next;
         }
 
@@ -52,10 +67,35 @@ class LinkedList {
     }
 }
 
+function undefinedToNull(value) {
+    if (value === undefined) {
+        return null;
+    } else {
+        return value;
+    }
+}
+
 function testLinkedList() {
-    let listNew = new LinkedList();
-    let listCloned = listNew.clone();
-    console.log(listNew, listNew)
-    console.log(listNew === listCloned)
+    // Setup data in the linked list.
+    let root = new Node(5);
+    let list = new LinkedList(root);
+    list.push(4);
+    list.push(3);
+    list.push(2);
+    list.push(1);
+
+    // Setup random references.
+    list.head.random = list.head.next.next;
+    list.head.next.random =
+        list.head.next.next.next;
+    list.head.next.next.random =
+        list.head.next.next.next.next;
+    list.head.next.next.next.random =
+        list.head.next.next.next.next;
+    list.head.next.next.next.random =
+        list.head.next;
+
+    let listCloned = list.clone();
+    console.log(list, listCloned)
 }
 testLinkedList();
