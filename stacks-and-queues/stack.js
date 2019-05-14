345c615510dbb5e74827ce543456458dcdd5f3dc
Martin Zaklasnik <zaklas.martin@gmail.com>
2019-05-14 23:34:26 +0200

    Add stack class

---
+++ stacks-and-queues/stack.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Stack
+ * Time Complexity: O(1)
+ */
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+}
