035cfe769a9c98a265d6190705fe9462874ca697
xkubista <xkubista@gmail.com>
2019-05-27 22:21:11 +0200

    Add pattern multiple pointers countUniqueValues no order

--- src/patterns/multiple-pointers-countUniqueValues.js
+++ src/patterns/multiple-pointers-countUniqueValues.js
@@ -38,3 +38,17 @@ countUniqueValues([1,1,1,1,1,2]) // 2
 countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
 countUniqueValues([]) // 0
 countUniqueValues([-2,-1,-1,0,1]) // 4
+
+function countUniqueValuesNoOrder(arr) {
+    let tmp = {};
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (!tmp[arr[i]]) {
+            count++;
+            tmp[arr[i]] = true;
+        }
+    }
+    return count;
+}
+
+countUniqueValuesNoOrder([3,4,3,3,2,2,6,1,5,1]); // 6
