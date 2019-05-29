f60c752fa321f0bd214371e59f9b882cbf6042d5
Jakub Kubišta <xkubista@gmail.com>
2019-05-29 12:09:35 +0200

    Refactor remove duplicates

--- src/strings/remove-duplicates.js
+++ src/strings/remove-duplicates.js
@@ -8,27 +8,35 @@
  * Source: https://www.geeksforgeeks.org/remove-duplicates-from-a-given-string/
  * @param {string} str
  */
+
 function removeDuplicates(str) {
     let newStr = '';
-    // Array passage - O(n)
-    for (let i = 0; i < str.length; i++) {
-        let character = str.charAt(i);
-        // Search character index of string - O(n)
-        if (newStr.indexOf(character) < 0) {
-            newStr += character;
+    let uniques = {};
+    for (let char of str) {
+        if (!uniques[char]) {
+            uniques[char] = true;
+            newStr += char;
         }
+    }
+
+    return newStr;
+}
+
+
+function removeDuplicatesSet(str) {
+    let set =  new Set(str);
+    let newStr = '';
+    for (let char of set) {
+        newStr += char;
     }
     return newStr;
 }
 
+
 function testRemoveDuplicates() {
     let expected = 'geksfor';
     let str = "geeksforgeeks";
-    console.log(removeDuplicates(str))
-    if (removeDuplicates(str) === expected) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
+    console.log(removeDuplicates(str));
+    console.log(removeDuplicates(str) === expected);
 }
 testRemoveDuplicates();
