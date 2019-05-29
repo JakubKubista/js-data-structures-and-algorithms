29d362dc7348f0aca7935e95a710656d9ec20fa4
Jakub Kubišta <xkubista@gmail.com>
2019-05-29 23:10:08 +0200

    Add an example for helper method recursion

--- src/patterns/helper-method-recursion.js
+++ src/patterns/helper-method-recursion.js
@@ -4,15 +4,27 @@
  * Pattern: Helper Method Recursion.
  * Source: https://www.udemy.com/js-algorithms-and-data-structures-masterclass/learn/lecture/9816154
  * Description: Function outer contains recursive function helper to compile Array or Data Structure.
+ *
+ * Task: Write a function to collect odd values from an array.
  */
 
-function outer(input) {
-  let outerScoped = [];
+function collectOddValues(arr) {
+  let result = [];
 
   function helper(helperInput) {
-    // modify outerScoped variable
-    helper(helperInput--);
+    if (helperInput.length === 0) {
+      return;
+    }
+
+    if (helperInput[helperInput.length-1] % 2 !== 0) {
+      result.unshift(helperInput[helperInput.length-1]);
+    }
+    helperInput.pop();
+    helper(helperInput);
   }
-  helper(input);
-  return outerScoped;
+  helper(arr);
+
+  return result;;
 }
+
+collectOddValues([1,2,3,4,5,6,7,8,9]);
