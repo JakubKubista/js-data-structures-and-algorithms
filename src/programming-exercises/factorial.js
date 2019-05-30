ad79602c7dbd86558fa71bcb704c6cff0ab7953c
kubistaja <kubista.jakub@allrisk.cz>
2019-05-30 09:01:11 +0200

    Refactor factorial

--- src/programming-exercises/factorial.js
+++ src/programming-exercises/factorial.js
@@ -8,7 +8,7 @@
 
 function factorial(num) {
     if (num === 0) return 1;
-    return num * Factorial(num - 1);
+    return num * factorial(num - 1);
 }
 function factorialLoop(num) {
     let total = 1;
