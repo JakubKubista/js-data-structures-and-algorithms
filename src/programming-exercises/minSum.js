e3085fa5c800c306540242fc9629b67d8f0cc15e
Jakub Kubišta <xkubista@gmail.com>
2019-06-02 18:47:29 +0200

    Add exercise minSum

---
+++ src/programming-exercises/minSum.js
@@ -0,0 +1,92 @@
+/**
+ * Min sum
+ * Description: The function returns the sum of the array "num".
+ * For each "k" it is necessary to find the maximum value in the array
+ * and to substitute it by a 2-times smaller value (rounded up).
+ */
+
+// First brute force ES5 solution
+
+
+// Time Complexity: O(n*n)
+function minSumES5(num, k) {
+  if (k === 0) return 0;
+  let value = Math.max(...num);
+  let i = num.indexOf(value);
+  num[i] = Math.round(value/2);
+  minSumES5(num, --k);
+  console.log(num);
+  const add = (a, b) => a + b;
+  return num.reduce(add);
+}
+
+function findInsertNode(num) {
+  let min = 0;
+  let max = num.length - 1;
+  let middle = 0;
+  let val = Math.round(num[max]/2);
+  while (min <= max) {
+    middle = Math.floor((min + max) / 2);
+    if (num[middle] < val) {
+        min = middle + 1;
+    } else if (num[middle] > val) {
+        max = middle - 1;
+        if(max >= 0) return {middle, val};
+    } else {
+        return {middle, val};
+    }
+  }
+}
+
+// Time Complexity: O(n log n)
+function minSum(num, k) {
+  let i = num.length;
+  let node = {};
+  while (k > 0) {
+      if (i === 0 || i === num.length) {
+          num.sort(function(a, b){return a - b});
+          i = num.length;
+      }
+      node = findInsertNode(num);
+      num.pop();
+      num.splice(node.middle, 0, node.val);
+      --k;
+      --i;
+  }
+  const add = (a, b) => a + b;
+  return num.reduce(add);
+}
+
+let num = [10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,
+  41,245,6548,215,354,5,8489,6846,648864,86468,
+  684684,864,864,684,684,684,684,6846,848,10,20,7,5,
+  7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,6548,215,354,5,8489,
+  6846, 648864, 86468, 684684,864 ,864 ,684 ,684,684,684,
+  6846,848,10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,
+  6548,215,354, 5, 8489, 6846, 648864, 86468, 684684,864,
+  864 ,684 ,684,684,684,6846,848,
+  10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,
+  41,245,6548,215,354,5,8489,6846,648864,86468,
+  684684,864,864,684,684,684,684,6846,848,10,20,7,5,
+  7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,6548,215,354,5,8489,
+  6846, 648864, 86468, 684684,864 ,864 ,684 ,684,684,684,
+  6846,848,10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,
+  6548,215,354, 5, 8489, 6846, 648864, 86468, 684684,864,
+  864 ,684 ,684,684,684,6846,848,
+  10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,
+  41,245,6548,215,354,5,8489,6846,648864,86468,
+  684684,864,864,684,684,684,684,6846,848,10,20,7,5,
+  7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,6548,215,354,5,8489,
+  6846, 648864, 86468, 684684,864 ,864 ,684 ,684,684,684,
+  6846,848,10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,
+  6548,215,354, 5, 8489, 6846, 648864, 86468, 684684,864,
+  864 ,684 ,684,684,684,6846,848];
+let k = 4;
+console.log(num);
+function runPerformanceTest() {
+  let t0 = performance.now();
+  console.log(minSum(num, k));
+  let t1 = performance.now();
+  console.log("Time Measured: " + Math.round((t1 - t0) * 100) / 100 + " ms.")
+}
+runPerformanceTest();
