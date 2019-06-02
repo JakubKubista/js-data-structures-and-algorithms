b81861e50fd702108124a8193f65281bdd3bf97d
Jakub Kubišta <xkubista@gmail.com>
2019-06-02 21:33:54 +0200

    Refactoring exercise min sum, documentation

--- src/programming-exercises/minSum.js
+++ src/programming-exercises/minSum.js
@@ -20,39 +20,62 @@ function minSumES5(num, k) {
   return num.reduce(add);
 }
 
+/**
+ * Binary search to find a node, where should be placed rounded max
+ * value divided by 2.
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ * @param {Array} num
+ * @returns object with index and value
+ */
+
 function findInsertNode(num) {
   let min = 0;
   let max = num.length - 1;
   let middle = 0;
-  let val = Math.round(num[max]/2);
+  let value = Math.round(num[max]/2);
   while (min <= max) {
     middle = Math.floor((min + max) / 2);
-    if (num[middle] < val) {
+    if (num[middle] < value) {
         min = middle + 1;
-    } else if (num[middle] > val) {
+    } else if (num[middle] > value) {
         max = middle - 1;
-        if(max >= 0) return {middle, val};
+        if(max >= 0) return {middle, value};
     } else {
-        return {middle, val};
+        return {index: middle, value};
     }
   }
 }
 
-// Time Complexity: O(n log n)
+/**
+ * The main function for iteration above k.
+ *
+ * Time Complexity: O(n log n) - longer array, O(n * n) - shorter array
+ * Space Complexity: O(log(n)- longer array, O(1) - shorter array
+ * @param {Array} num
+ * @param {Number} k
+ * @returns number
+ */
+
 function minSum(num, k) {
   let i = num.length;
   let node = {};
+  // Sort an array to reach less complexity.
+  num.sort(function(a, b){return a - b});
   while (k > 0) {
       if (i === 0 || i === num.length) {
           num.sort(function(a, b){return a - b});
           i = num.length;
       }
+      // Search the biggest node at the end.
       node = findInsertNode(num);
       num.pop();
-      num.splice(node.middle, 0, node.val);
+      num.splice(node.index, 0, node.value);
       --k;
       --i;
   }
+  // Get sum of numbers and return.
   const add = (a, b) => a + b;
   return num.reduce(add);
 }
@@ -81,7 +104,8 @@ let num = [10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,
   6846,848,10,20,7,5,7,8,9,1,0,2,1,4,5,7,4,5,6,8,7,41,245,
   6548,215,354, 5, 8489, 6846, 648864, 86468, 684684,864,
   864 ,684 ,684,684,684,6846,848];
-let k = 4;
+let k = 10000000;
+
 console.log(num);
 function runPerformanceTest() {
   let t0 = performance.now();
