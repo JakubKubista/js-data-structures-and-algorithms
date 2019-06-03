3f97f25562dd87e82fa3fc40f6385d932d78ad9e
Jakub Kubišta <xkubista@gmail.com>
2019-06-03 22:41:15 +0200

    Add palindrome variations

--- src/strings/palindrome.js
+++ src/strings/palindrome.js
@@ -5,6 +5,21 @@
  * Time Complexity: O(n / 2)
  * @param {string} str
  */
+
+
+function isPalindromeLoop(s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== s[s.length-1-i]) return false;
+    }
+    return true;
+}
+
+function isPalindrome(s) {
+    if (s.length < 2) return true;
+    if (s[0] !== s[s.length-1]) return false;
+    return isPalindrome(s.substring(1,s.length-1));
+}
+
 function isPalindrome(str) {
     if(str.length === 1) return true;
     if(str.length === 2) return str[0] === str[1];
@@ -13,5 +28,10 @@ function isPalindrome(str) {
 }
 
 function test() {
-    console.log(isPalindrome('isomorphic'))
+    console.log(isPalindrome('isomorphic')); // false
+    console.log(isPalindrome('foobar')); // false
+    console.log(isPalindrome('tacocat')); // true
+    console.log(isPalindrome('amanaplanacanalpanama')); // true
+    console.log(isPalindrome('amanaplanacanalpandemonium')); // false
 }
+test();
